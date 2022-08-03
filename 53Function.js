613c66cfeafb8e3f617d80fcbcd25b51cb26635b
Chintan Patel <patelchintan843@gmail.com>
2022-08-03 21:26:07 +0530

    Initial commit

---
+++ 53Function.js
@@ -0,0 +1,14 @@
+console.log("hello");
+
+
+function greet(name){
+    console.log(name + " is a good boy");
+}
+let name1 = "Chintan";
+let name2 = "Sumit";
+let name3 = "Krunal";
+let name4 = "Hemang";
+greet(name1);
+greet(name2);
+greet(name3);
+greet(name4);
